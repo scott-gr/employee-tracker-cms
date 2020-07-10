ff8fd6462ccfe187d672c4badeeef596ae192e64
Scott Griffin <mscottgriffin@gmail.com>
2020-07-09 22:27:19 -0400

    wrote 3 tabled in schema. Need to work on reference fields

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS office_cmsDB;
+CREATE database office_cmsDB;
+
+USE office_cmsDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE `role` (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL (7,2) NULL,
+  department_id INT NOT NULL, ---reference
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NOT NULL, ---reference
+  manager_id INT NOT NULL ---reference
+  PRIMARY KEY (id)
+);
+
+-- resolve reference fields
