fadaac4959820d14fcadf88ae853c10d184e7037
Scott Griffin <mscottgriffin@gmail.com>
2020-07-11 01:13:31 -0400

    wrote main menu choice-function if-then. Will put functions in orm.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //required npm packages
-const orm = require("./config/orm");
+const orm = require('./config/orm');
 
 //add department
 
@@ -11,13 +11,59 @@ const orm = require("./config/orm");
 //view role
 //view employee
 
+function init() {
+  inquirer
+    ////////choices of functions in the program
+    .prompt([
+      {
+        type: 'list',
+        choices: [
+          'View All Employees',
+          'View All Roles',
+          'View All Departments',
+          'Add Employee',
+          'Add Role',
+          'Add Department',
+          'Update Employee Role',
+          'Remove Employee',
+          'Remove Role',
+          'Remove Department',
+          // "Update Employee Manager",
+          // "Delete Department",
+          // "Delete Role",
+          // "Delete Employee"
+        ],
+        name: 'choice',
+        message: 'What do you want to do?',
+      },
+    ])
+    .then(({ choice }) => {
+      if (choice === 'Add Employee') {
+        orm.addNewEmployee();
+      } else if (choice === 'Add Role') {
+        orm.addNewRole();
+      } else if (choice === 'Add Department') {
+        orm.addNewDepartment();
+      } else if (choice === 'Update Employee Role') {
+        orm.updateEmployeeRole();
+      } else if (choice == 'View All Employees') {
+        orm.viewAllEmployees();
+      } else if (choice == 'View All Roles') {
+        orm.viewAllRoles();
+      } else if (choice == 'View All Departments') {
+        orm.viewAllRoles();
+      } else {
+        orm.exit();
+      }
+    });
+}
 
 
 
 const app = {
-  init:init
-}
+  init: init,
+};
 
 module.exports = {
-  app:app,
-}
+  app: app,
+};
--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,37 @@
 const connection = require("./connection");
 
-function init() {
-  inquirer
-  ////////choices of functions in the program
-    .prompt([
-      {
-        type: "list",
-        choices: [
-          "View All Employees",
-          "View All Roles",
-          "View All Departments",
-          "Add Employee",
-          "Add Role",
-          "Add Department",
-          "Remove Employee",
-          "Update Employee Role",
-          // "Update Employee Manager", 
-          // "Delete Department",
-          // "Delete Role",
-          // "Delete Employee"
-        ],
-        name: "choice",
-        message: "What do you want to do?",
-      },
-    ])
+///sample orm code
+    // var orm = {
+    //   selectWhere: function(tableInput, colToSearch, valOfCol) {
+    //     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    //     connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+    //       if (err) throw err;
+    //       console.log(result);
+    //     });
+    //   },
+    //   selectAndOrder: function(whatToSelect, table, orderCol) {
+    //     var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+    //     console.log(queryString);
+    //     connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+    //       if (err) throw err;
+    //       console.log(result);
+    //     });
+    //   },
+    //   findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    //     var queryString =
+    //       "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
     
+    //     connection.query(
+    //       queryString,
+    //       [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+    //       function(err, result) {
+    //         if (err) throw err;
+    //         console.log(result);
+    //       }
+    //     );
+    //   }
+    // };
+    
+
 
 module.exports = orm;
