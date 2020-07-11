389ba369b880e94076cb7756a57bef77a074dbff
Scott Griffin <mscottgriffin@gmail.com>
2020-07-10 21:39:41 -0400

    created seeds file, linked orm-connection-app

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 //required npm packages
-const mysql = require("mysql");
-const cTable = require('console.table');
-const inquirer = require("inquirer");
+const orm = require("./config/orm");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,  // Your port; if not 3306
-  user: "root",  // Your username
-  password: "Gemma2020",   // Your password
-  database: "" //database name
-});
+//add department
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Success! Connected as id: " + connection.threadId);
-  connection.end();
-});
+//add role
+
+//add employee
+
+//view department
+//view role
+//view employee
+
+
+
+
+const app = {
+  init:init
+}
+
+module.exports = {
+  app:app,
+}
