83bf7aca940ce7823082011e0dda48b69fbbb87f
Scott Griffin <mscottgriffin@gmail.com>
2020-07-10 23:18:45 -0400

    added prompts for basic functions

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,29 @@
 const connection = require("./connection");
 
+function init() {
+  inquirer
+  ////////choices of functions in the program
+    .prompt([
+      {
+        type: "list",
+        choices: [
+          "View All Employees",
+          "View All Roles",
+          "View All Departments",
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+          "Remove Employee",
+          "Update Employee Role",
+          // "Update Employee Manager", 
+          // "Delete Department",
+          // "Delete Role",
+          // "Delete Employee"
+        ],
+        name: "choice",
+        message: "What do you want to do?",
+      },
+    ])
+    
 
 module.exports = orm;
