886dfc4bd7821789a63de2f6664297b89793b5e5
Scott Griffin <mscottgriffin@gmail.com>
2020-07-12 23:45:34 -0400

    updated role functions

--- config/orm.js
+++ config/orm.js
@@ -7,18 +7,18 @@ var orm = {
     inquirer.prompt([
       {
         type: 'input',
-        name: 'first_name',
-        message: 'Please enter the first name of the employee:'
+        name: 'title',
+        message: 'Please provide a job title for this new role:'
       },
       {
         type: 'input',
-        name: 'last_name',
-        message: 'Please enter the last name of the employee:'
+        name: 'salary',
+        message: 'Please provide the salary for the new role:'
       },
       {
         type: 'input',
-        name: 'role_id',
-        message: 'Please enter the role id of the new employee:'
+        name: 'department_id',
+        message: 'Please enter the department ID that includes this new role:'
       },
 
     ]).then(employeeList => {
@@ -83,20 +83,20 @@ var orm = {
   removeRole: () => {
     connection.query('SELECT * FROM role', (err, eData) => {
       if (err) throw err;
-      let selectedEmployee = eData.map(employee => `${employee.id} ${employee.first_name} ${employee.last_name}`);
+      let selectedRole = eData.map(role => `${role.id} ${role.title} ${role.salary}`);
       inquirer.prompt([
         {
           type: 'list',
-          message: 'Which employee should be removed from the database?',
-          choices: selectedEmployee,
-          name: 'employee'
+          message: 'Which role should be removed from the database?',
+          choices: selectedRole,
+          name: 'role'
         }
       ]).then(output => {
-        let employeeId = output.employee.slice(0,2);
-        let employeeName = output.employee.slice(3);
-        connection.query('DELETE FROM employee WHERE id = ?', [employeeId], err => {
+        let roleId = output.role.slice(0,2);
+        let roleTitle = output.role.slice(3);
+        connection.query('DELETE FROM role WHERE id = ?', [roleId], err => {
           if (err) throw err;
-          console.log(`${employeeName} has beeen removed.`);
+          console.log(`${roleTitle} has beeen removed.`);
           init();
         });
       });
